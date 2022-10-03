e12f80bebd857eca542e73a3a395af41829bfbca
Chris Newbold <chris@newboldcreative.com>
2022-10-03 20:10:34 +1100

    update resume margin

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -4,16 +4,14 @@ const Project = ({ data }) => {
   return (
     <section id="portfolio" className="display">
       <div>
-        <div>
-          <h3 className="project-title">{data.name}</h3>
-          <p>{data.url}</p>
-          <p>{data.repo}</p>
-          <img
-            src={require(`../../assets/images/portfolio/${data.feature}.png`)}
-            alt={data.name}
-            key={data.name}
-          />
-        </div>
+        <h3 className="project-title">{data.name}</h3>
+        <p>{data.url}</p>
+        <p>{data.repo}</p>
+        <img
+          src={require(`../../assets/images/portfolio/${data.feature}.png`)}
+          alt={data.name}
+          key={data.name}
+        />
       </div>
     </section>
   );
